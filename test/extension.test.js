d2bfe204734102a340c7e4a782238069417bde79
Marek Lewandowski <marek@mlewandowski.com>
2017-03-23 23:50:28 +0100

    Covered inline functions.

--- test/extension.test.js
+++ test/extension.test.js
@@ -20,6 +20,73 @@ suite( "Bold", function() {
     } );
 } );
 
+suite( "Italic", function() {
+    test( "Ranged selection", function() {
+        return TestCommand( 'toggleItalic', 'Lets make a [fancy} text!', 'Lets make a [_fancy_} text!' );
+    } );
+
+    test( "Collapsed selection", function() {
+        // This is likely to be changed with #5.
+        return TestCommand( 'toggleItalic', 'Lets make a fan^cy text!', 'Lets make a fan_^_cy text!' );
+    } );
+
+    test( "Toggles with ranged selection", function() {
+        return TestCommand( 'toggleItalic', 'Lets make less [_fancy_} text', 'Lets make less [fancy} text' );
+    } );
+} );
+
+suite( "Strike through", function() {
+    test( "Ranged selection", function() {
+        return TestCommand( 'toggleStrikethrough', 'Lets make a [fancy} text!', 'Lets make a [~~fancy~~} text!' );
+    } );
+
+    test( "Collapsed selection", function() {
+        // This is likely to be changed with #5.
+        return TestCommand( 'toggleStrikethrough', 'Lets make a fan^cy text!', 'Lets make a fan~~^~~cy text!' );
+    } );
+
+    test( "Toggles with ranged selection", function() {
+        return TestCommand( 'toggleStrikethrough', 'Lets make less [~~fancy~~} text', 'Lets make less [fancy} text' );
+    } );
+} );
+
+suite( "Inline code", function() {
+    test( "Ranged selection", function() {
+        return TestCommand( 'toggleInlineCode', 'Lets make a [fancy} text!', 'Lets make a [`fancy`} text!' );
+    } );
+
+    test( "Collapsed selection", function() {
+        // This is likely to be changed with #5.
+        return TestCommand( 'toggleInlineCode', 'Lets make a fan^cy text!', 'Lets make a fan`^`cy text!' );
+    } );
+
+    test( "Toggles with ranged selection", function() {
+        return TestCommand( 'toggleInlineCode', 'Lets make less [`fancy`} text', 'Lets make less [fancy} text' );
+    } );
+} );
+
+suite( "Headers", function() {
+    // For headers we'll generate the tests, so that this test suite doesn't get too bloat.
+    for ( let i = 1; i <= 6; i++ ) {
+        suite( 'Level ' + i, function() {
+            var headerMarker = '#'.repeat( i );
+
+            test( "Ranged selection", function() {
+                return TestCommand( `toggleTitleH${i}`, 'Lets make a [fancy} text!', `Lets make a [${headerMarker} fancy} text!` );
+            } );
+
+            test( "Collapsed selection", function() {
+                // This is likely to be changed with #5.
+                return TestCommand( `toggleTitleH${i}`, 'Lets make a fan^cy text!', `Lets make a fan${headerMarker} ^cy text!` );
+            } );
+
+            test( "Toggles with ranged selection", function() {
+                return TestCommand( `toggleTitleH${i}`, `Lets make less [${headerMarker} fancy} text`, 'Lets make less [fancy} text' );
+            } );
+        } );
+    }
+} );
+
 // A helper function that generates test case functions.
 // Both inputContent and expectedContent can include selection string representation.
 // Returns a promise resolving to Promise<TextEditor>.
